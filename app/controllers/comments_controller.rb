020fe71c0941319ae741c76b5c4ce295617ac175
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-11-01 18:25:32 -0700

    update comments controller with edit and update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,23 +11,58 @@ class CommentsController < ApplicationController
 	def new
 		@haunt = Haunt.find(params[:haunt_id])
 		@comment = current_user.comments.build
-		# @comment = Comment.new
 	end
 
 
 	def create
 		@haunt = Haunt.find(params[:haunt_id])
-		# @comment = current_user.comments.create(comment_params)
 		@comment = current_user.comments.create(comment_params.merge(user_id: current_user.id))
+		if @comment.content.blank? 
+			flash[:error] = "Oops!  You didn't fill out your review."
+			redirect_to new_haunt_comment_path
+		else
     	# comment = comments.build(comment_params)
     	# binding.pry
     	redirect_to haunt_comment_path(@haunt, @comment)
+    	end
   	end
 
   	def show
   		@haunt = Haunt.find(params[:haunt_id])
   		@comment = Comment.find(params[:id])
+  	end
 
+  	def edit 
+  		@haunt = Haunt.find(params[:haunt_id])
+		@comment = Comment.find(params[:id])
+		# if @comment && @comment.user.email == current_user
+
+		# else
+    		# flash[:error] = "Foolish mortal. You can only edit your own entries!"
+    		# redirect_to comments_path
+    	# end
+	end
+
+	def update
+		@comment = Comment.find(params[:id])
+		if @comment && @comment.user.email == current_user
+		@comment.update(comment_params)
+		flash[:notice] = "Success!"
+		redirect_to comments_path
+		end
+	end
+
+	 def destroy
+	 	@comment = Comment.find(params[:id])
+	 	if @comment && @comment.user.email == current_user
+    		@comment.destroy
+    		redirect_to comments_path
+  
+    	else
+    		flash[:error] = "Foolish mortal. You can only delete your own entries!"
+    		redirect_to comments_path
+  
+    	end
   	end
 
 	private
