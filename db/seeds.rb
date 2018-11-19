60e914408851d9b63ea0157e697ae654bc74acdc
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-11-19 15:13:02 -0800

    add basic seed data

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Haunt.destroy_all
+ 
+Haunt.create!(name: "Haunted Mansion",
+              location: "Disneyland",
+              description: "The Haunted Mansion is a dark ride attraction located at Disneyland Park (Disneyland Resort), Magic Kingdom, and Tokyo Disneyland. The attraction, although differing slightly in every location, places riders inside a haunted manor resided by '999 happy haunts'."
+)
+
+Haunt.create!(name: "The Winchester Mystery House",
+              location: "San Jose, CA",
+              description: "The Winchester Mystery House is a mansion in San Jose, California, that was once the personal residence of Sarah Winchester, the widow of firearm magnate William Wirt Winchester. Located at 525 South Winchester Blvd. in San Jose, the Queen Anne Style Victorian mansion is renowned for its size, its architectural curiosities, and its lack of any master building plan. It is a designated California historical landmark and is listed on the National Register of Historic Places. It is privately owned and serves as a tourist attraction."
+)
