16638d11d35bd3b54be0d22f14b086b3decaf81d
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2018-12-18 14:10:19 -0800

    clean up unused code

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ApplicationRecord
 	belongs_to :user, optional: true
 	belongs_to :haunt, optional: true
 
-	accepts_nested_attributes_for :user, reject_if: :all_blank
-	accepts_nested_attributes_for :haunt
+	# accepts_nested_attributes_for :user, reject_if: :all_blank
+	# accepts_nested_attributes_for :haunt
 
 	# Move validations here
 	validates :content, presence: true
