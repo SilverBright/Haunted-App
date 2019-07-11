1820ef5c0d88ec56305031d13ff62b11395553fb
github username <barbara.m.washington@gmail.com>
2019-07-10 20:55:06 -0700

    add bootstrap styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
 authenticated :user do
-    root 'home#index', as: 'authenticated_root'
+    root 'haunts#index', as: 'authenticated_root'
   end
   devise_scope :user do
     root 'devise/sessions#new'
