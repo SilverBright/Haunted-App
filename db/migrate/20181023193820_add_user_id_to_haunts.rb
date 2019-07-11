f65e4355fcc959f43b84d102f7d77c3f9cb23f8a
github username <barbara.m.washington@gmail.com>
2019-07-11 13:37:37 -0700

    fix login issue with oauth token and user_id

--- db/migrate/20181023193820_add_user_id_to_haunts.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToHaunts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :haunts, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_193820) do
+ActiveRecord::Schema.define(version: 2018_10_18_205235) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2018_10_23_193820) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
