71adac5275deafbc78a346046b473ded793b9b10
github username <barbara.m.washington@gmail.com>
2019-07-11 14:03:28 -0700

    change seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,26 @@ Haunt.create!(name: "The Stanley Hotel",
 Haunt.create!(name: "Winchester Mystery House",
               city: "San Jose",
               state: "CA",
-              description: "The Winchester Mystery House is a mansion in San Jose, California, that was once the personal residence of Sarah Winchester, the widow of firearm magnate William Wirt Winchester. Located at 525 South Winchester Blvd. in San Jose, the Queen Anne Style Victorian mansion is renowned for its size, its architectural curiosities, and its lack of any master building plan. It is a designated California historical landmark and is listed on the National Register of Historic Places. It is privately owned and serves as a tourist attraction."
+              description: "The Winchester Mystery House is a mansion in San Jose, California, that was once the personal residence of Sarah Winchester, the widow of firearm magnate William Wirt Winchester."
 )
 
 Haunt.create!(name: "Pittock Mansion",
               city: "Portland",
               state: "OR",
-              description: "A 22-room French chateau in the hills overlooking downtown Portland built in 1922, the Pittock Mansion was the site of only two deaths (original residents Georgiana [1918; age 72] and Henry Pittock [1919; age 84]) and one political scandal (Pittock was able to get a water line installed to the mansion at the city's expense, despite the property being outside city limits), but since being opened to the public in 1965, it's been host to a number of ghost sightings, including floating old ladies, boots walking without legs, portraits moving of their own accord, windows opening and shutting themselves, and, perhaps most improbably, a tree with a face in it. The Mansion was also where the 1982 slasher film Unhinged and Madonna's 1993 answer-to-Sharon-Stone's-Basic-Instinct clunker Body of Evidence were filmed."
+              description: "A 22-room French chateau in the hills overlooking downtown Portland built in 1922, and was the site of only two deaths and one political scandal, but since being opened to the public in 1965, it's been host to a number of ghost sightings."
 )
 
-Comment.create!(user_id: 1, haunt_id: 1, content: 'Very spooky place!')
-Comment.create!(user_id: 2, haunt_id: 2, content: 'My kids love coming here every halloween')
-Comment.create!(user_id: 3, haunt_id: 3, content: 'Beautiful location, but not very scary!')
+Haunt.create!(name: "Haunted Mansion",
+              city: "Anaheim",
+              state: "CA",
+              description: "Is a dark ride attraction located at Disneyland Park (Disneyland Resort), Magic Kingdom, and Tokyo Disneyland. The attraction, although differing slightly in every location, places riders inside a haunted manor resided in by '999 happy haunts'."
+)
+
+Comment.create!(user_id: 1, haunt_id: 1, content: "Very spooky place!")
+Comment.create!(user_id: 2, haunt_id: 2, content: "My kids love coming here every halloween")
+Comment.create!(user_id: 3, haunt_id: 3, content: "Beautiful location, but not very scary!")
+Comment.create!(user_id: 1, haunt_id: 4, content: "We visit Disneyland every year. This is my favorite attraction!")
+Comment.create!(user_id: 2, haunt_id: 1, content: "I'm a big fan of the Shining movie. It's really neat to see the hotel in person.")
+Comment.create!(user_id: 1, haunt_id: 4, content: "Love the grim grinning ghosts.")
+Comment.create!(user_id: 1, haunt_id: 2, content: "LOVED the midnight flashlight tour!")
+Comment.create!(user_id: 2, haunt_id: 3, content: "Wow, what a neat place!!")
